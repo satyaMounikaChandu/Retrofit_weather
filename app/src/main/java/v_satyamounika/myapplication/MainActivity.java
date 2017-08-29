1b220414fc12a4c18e53c5bc4dabb854c4b3ccd3
Satya Chandu <satya.chandu@redbox.com>
2017-08-29 09:38:21 -0500

    Movies Commit2

--- app/src/main/java/v_satyamounika/myapplication/MainActivity.java
+++ app/src/main/java/v_satyamounika/myapplication/MainActivity.java
@@ -1,14 +1,22 @@
 package v_satyamounika.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final static String API_KEY = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (API_KEY.isEmpty()) {
+            Toast.makeText(getApplicationContext(), "Please obtain your API KEY first from themoviedb.org", Toast.LENGTH_LONG).show();
+            return;
+        }
 
 
     }
